836cddca0ac2b2d7ffcda3c0821bb0ceae0206f3
NHappyFox <eduardowna2020@yandex.ru>
2021-02-02 00:49:26 +0800

    Начала выполнение домашнего задания ко второму уроку.

--- HomeWorks/src/ru/geekbrains/evstigneeva/Main.java
+++ HomeWorks/src/ru/geekbrains/evstigneeva/Main.java
@@ -1,7 +1,27 @@
 package ru.geekbrains.evstigneeva;
 
 public class Main {
+
+
     public static void main(String[] args) {
 
+        arrayTwoSize(new String[4][4]);
+
+
     }
+
+    public static void arrayTwoSize(String[][] arrayTwoD) {
+
+        if (arrayTwoD.length == 4) {
+            if (arrayTwoD[0].length == 4) {
+                System.out.println("Хорошо");
+                return;
+            }
+        }
+
+        System.out.println("Плохо");
+
+
+    }
+
 }
