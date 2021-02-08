8ff6d543f109fc1f13562981d04f3e680febfee0
NHappyFox <eduardowna2020@yandex.ru>
2021-02-09 00:59:51 +0800

    Домашнее задание к четвёртому уроку выполнено.Осталось доделать задания 4,5,6. Выполню до вечера завтрашнего дня.

--- HomeWorks/src/ru/geekbrains/evstigneeva/Main.java
+++ HomeWorks/src/ru/geekbrains/evstigneeva/Main.java
@@ -1,7 +1,48 @@
 package ru.geekbrains.evstigneeva;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Chat chat = new Chat();
+
+        Integer[] list = {10, 2, 23, 44};
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i]);
+        }
+        int search = search(22, list);
+        System.out.println(search);
+        System.out.println();
+        String result = reverse("Всё реально");
+        System.out.println(result);
+        System.out.println();
+        System.out.println(maximum(list));
+
+
+    }
+
+    public static int search(Integer number, Integer[] list) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] == number) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static String reverse(String one) {
+
+        StringBuilder stringBuilder = new StringBuilder(one);
+        stringBuilder.reverse();
+        return stringBuilder.toString();
+    }
+
+    public static Integer maximum(Integer[] list) {
+
+
+        return null;
 
     }
 }
