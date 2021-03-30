4380a845f918d897bca01e52aed82f63f9a0e5d3
NHappyFox <eduardowna2020@yandex.ru>
2021-03-30 17:07:21 +0800

    Домашнее задание для 1-ого урока часть 1 - только начала выполнение.

--- HomeWorks/src/ru/geekbrains/evstigneeva/Main.java
+++ HomeWorks/src/ru/geekbrains/evstigneeva/Main.java
@@ -6,13 +6,13 @@ public class Main {
     public static void main(String[] args) {
 
         String[] arr = {"asd", "ds", "ret", "123"};
-        System.out.println("Swap a places: " + Arrays.toString(arr));
-        swapElements(arr, 0, 3);
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Original array: " + Arrays.toString(arr));
+        swapIndexs(arr, 0, 3);
+        System.out.println("Did swap a places: " + Arrays.toString(arr));
 
     }
 
-    private static <T> void swapElements(T[] array, int index_1, int index_2) {
+    private static <T> void swapIndexs(T[] array, int index_1, int index_2) {
         T temp = array[index_1];
         array[index_1] = array[index_2];
         array[index_2] = temp;
